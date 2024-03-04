3648a4655e765f4d510b84dfdc2e43b1d747ba0e
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2024-03-04 00:07:46 -0500

    schema and seed data finished

--- db/schema.sql
+++ db/schema.sql
@@ -1,44 +1,55 @@
-DROP DATABASE IF EXISTS goalhive_dev;
-CREATE DATABASE goalhive_dev;
-\c goalhive_dev;
-CREATE TABLE user_profile
-(
-    user_profile_id SERIAL PRIMARY KEY,
+DROP DATABASE IF EXISTS goalhive_app;
+CREATE DATABASE goalhive_app;
+\c goalhive_app;
+
+CREATE TABLE user_accounts (
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE user_profiles (
+    user_profile_id SERIAL PRIMARY KEY,
     firstname VARCHAR,
     lastname VARCHAR,
-    email VARCHAR(255) NOT NULL UNIQUE,
     user_profile_img VARCHAR(255),
     age INTEGER,
     gender VARCHAR,
     bio VARCHAR(255),
-    password_hash VARCHAR(255) NOT NULL
+    last_login TIMESTAMP,
+    active_status VARCHAR DEFAULT 'active'
 );
-CREATE TABLE interest (
+
+CREATE TABLE interests (
     interest_id SERIAL PRIMARY KEY,
     interest_name TEXT
 );
+
 CREATE TABLE goals (
     goal_id SERIAL PRIMARY KEY,
-    user_profile_id INTEGER REFERENCES user_profile(user_profile_id),
-    partner_user_id INTEGER REFERENCES user_profile(user_profile_id),
+    user_id INTEGER REFERENCES user_accounts(user_id),
+    partner_user_id INTEGER REFERENCES user_profiles(user_profile_id),
     goal_name VARCHAR(50),
     goal_description VARCHAR(255),
     target_date DATE,
+    meeting_frequency_preference VARCHAR(255),
+    goal_length_preference VARCHAR(255),
     creater_progress INTEGER,
-    partner_progress INTEGER, 
-    interest_id INTEGER REFERENCES interest(interest_id)
+    partner_progress INTEGER,
+    interest_id INTEGER REFERENCES interests(interest_id)
 );
 
-CREATE TABLE interest_connection (
+CREATE TABLE interest_connections (
     interest_connection_id SERIAL PRIMARY KEY,
-    interest_id INTEGER REFERENCES interest(interest_id),
-    user_profile_id INTEGER REFERENCES user_profile(user_profile_id)
+    interest_id INTEGER REFERENCES interests(interest_id),
+    user_profile_id INTEGER REFERENCES user_profiles(user_profile_id)
 );
-CREATE TABLE connection_request (
+
+CREATE TABLE connection_requests (
     request_id SERIAL PRIMARY KEY,
-    sender_id INTEGER REFERENCES user_profile(user_profile_id),
-    receiver_id INTEGER REFERENCES user_profile(user_profile_id),
+    sender_id INTEGER REFERENCES user_profiles(user_profile_id),
+    receiver_id INTEGER REFERENCES user_profiles(user_profile_id),
     status TEXT,
-    last_login TIMESTAMP
+    timestamp TIMESTAMP
 );
