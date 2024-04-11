66fd9b0a522dc6e4b69bd5552db2de8b0d25df3d
ShakaPursuit <shakalivngstone@pursuit.org>
2024-04-11 13:36:04 -0400

    fixed pghost

--- server.js
+++ server.js
@@ -453,7 +453,7 @@ const server = http.createServer(app);
 // Socket.IO configuration
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5174",
     methods: ["GET", "POST"],
   },
 });
