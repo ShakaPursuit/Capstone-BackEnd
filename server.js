1aed11e3722329230b2753970f76635d328b5a2a
JackieC1993 <jackiecheung@pursuit.org>
2024-04-18 23:30:03 -0400

    change the app.listen and server.listen js on server.js

--- server.js
+++ server.js
@@ -1,5 +1,140 @@
+// const express = require("express");
+// const app = require('./app')
+// const http = require("http");
+// const cors = require("cors");
+// const { Server } = require("socket.io");
+
+
+// require("dotenv").config();
+
+
+
+// // Express app configuration
+// app.use(cors());
+// app.use(express.json());
+// // const express = require("express");
+// // const app = require('./app')
+// // const http = require("http");
+// // const cors = require("cors");
+// // const { Server } = require("socket.io");
+
+
+// // require("dotenv").config();
+
+
+
+// // // Express app configuration
+// // app.use(cors());
+
+// const server = http.createServer(app);
+
+// // Socket.IO configuration
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5174",
+//     methods: ["GET", "POST"],
+//   },
+// });
+// const PORT = process.env.PORT;
+// // const PORT2= process.env.PORT2
+
+// io.on("connection", (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
+
+//   socket.on("send_message", (data) => {
+//     console.log(data)
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected", socket.id);
+//   });
+// });
+
+// // Start the server
+
+// app.listen, server.listen(PORT, () => {
+//   console.log(`Server is running on port: ${PORT}`);
+// });
+
+// // server.listen(PORT2, () => {
+// //   console.log(`Server is running on port: ${PORT2}`);
+// // });
+
+// const express = require("express");
+// const app = require('./app')
+// const http = require("http");
+// const cors = require("cors");
+// const { Server } = require("socket.io");
+
+
+// require("dotenv").config();
+
+
+
+// // Express app configuration
+// app.use(cors());
+// app.use(express.json());
+// // const express = require("express");
+// // const app = require('./app')
+// // const http = require("http");
+// // const cors = require("cors");
+// // const { Server } = require("socket.io");
+
+
+// // require("dotenv").config();
+
+
+
+// // // Express app configuration
+// // app.use(cors());
+
+// const server = http.createServer(app);
+
+// // Socket.IO configuration
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5174",
+//     methods: ["GET", "POST"],
+//   },
+// });
+// const PORT = process.env.PORT;
+// const PORT2= process.env.PORT2
+
+// io.on("connection", (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
+
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected", socket.id);
+//   });
+// });
+
+// // Start the server
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port: ${PORT}`);
+// });
+
+// server.listen(PORT2, () => {
+//   console.log(`Server is running on port: ${PORT2}`);
+// });
+
 const express = require("express");
-const app = require('./app')
+const app =  require('./app')
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
@@ -31,7 +166,7 @@ const server = http.createServer(app);
 // Socket.IO configuration
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
   },
 });
@@ -57,10 +192,10 @@ io.on("connection", (socket) => {
 
 // Start the server
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port: ${PORT}`);
+// });
 
-server.listen(PORT2, () => {
-  console.log(`Server is running on port: ${PORT2}`);
+app.listen && server.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
